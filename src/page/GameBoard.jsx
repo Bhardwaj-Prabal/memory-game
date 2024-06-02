cece4cd9cc8e480065daadd6b089b822a9e6f91f
Prabal Bhardwaj <125324867+Bhardwaj-Prabal@users.noreply.github.com>
2024-06-02 18:56:57 +0530

    Update GameBoard.jsx

--- src/page/GameBoard.jsx
+++ src/page/GameBoard.jsx
@@ -1,33 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../components/card";
 import { createUniqueRandomArray, shuffleArray } from "../components/RandomArray";
 import './GameBoard.css';
-import '../assets/pikachu.mp3';
 
-function GameBoard({currentScore,setScore,bestScore,setBestScore}) {
+function GameBoard({ currentScore, setScore, bestScore, setBestScore }) {
     const initialArr = createUniqueRandomArray(10, 1, 100);
 
     const [prevOrder, setOrder] = useState(initialArr);
     const [isClicked, setClicked] = useState([]);
+    const [audio, setAudio] = useState(null);
 
-    
+    useEffect(() => {
+        // Load the audio when the component mounts
+        const audioInstance = new Audio('/pikachu.mp3');
+        audioInstance.onloadeddata = () => {
+            setAudio(audioInstance);
+        };
+        audioInstance.onerror = (e) => {
+            console.error("Error loading audio: ", e);
+        };
+    }, []);
+
     function handleClick(id) {
-        function play() {
-            new Audio('pikachu.mp3').play();
-        }
-        
         if (isClicked.includes(id)) {
             setOrder(shuffleArray([...prevOrder]));
             setClicked([]);
-            if(currentScore>bestScore){
+            if (currentScore > bestScore) {
                 setBestScore(currentScore);
             }
             setScore(0);
         } else {
             setClicked([...isClicked, id]);
-            setScore(currentScore+1);    
+            setScore(currentScore + 1);
             setOrder(shuffleArray([...prevOrder])); // Shuffle the array and update the state
-            console.log(currentScore);
+        }
+
+        if (audio) {
+            audio.play().catch(error => console.error("Audio play failed: ", error));
         }
     }
 
@@ -38,7 +47,6 @@ function GameBoard({currentScore,setScore,bestScore,setBestScore}) {
                     key={value}
                     id={value}
                     onClick={() => handleClick(value)}
-                    
                 />
             ))}
         </div>
@@ -46,3 +54,4 @@ function GameBoard({currentScore,setScore,bestScore,setBestScore}) {
 }
 
 export default GameBoard;
+
